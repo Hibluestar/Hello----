69d3347cdf1aead0345249c712d26f7eee1eab34
Hibluestar <67965712+Hibluestar@users.noreply.github.com>
2020-07-08 13:53:58 -0400

    Add files via upload

---
+++ Hong's chatbot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+print ('Hello, how are you!')
+Name=input('What is your name?')
+print (Name,'is a great name, I love it!')
+age=input('How old are you?')
+print('Wow, I hope I know a lot by',age,'.')
+movie=input('What is your favorite movie?')
+print ('I love',movie,'too. It is a great movie!')
+food=input('What is your favorite food?')
+print('OH, that is yummy. I love', food,'as well.')
+print('It is nice to meet you,', Name, '. See you next time!')
+
